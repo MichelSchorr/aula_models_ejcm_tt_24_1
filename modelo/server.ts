e9775c6fa659f70fb83dc7920f38f21ae7eb0ec1
Christian Oliveira <christian.oliveira@ejcm.com.br>
2023-08-16 18:32:54 -0300

    Revertendo a cagada do Gave

---
+++ modelo/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import configDotenv from "./src/config/dotenv";
+// import router from "./src/routes/routes";
+// import jwtPassport from "./src/middlewares/jwtPassport";
+// import passport from "passport";
+// import cors from 'cors';
+
+configDotenv();
+// jwtPassport(passport);
+
+const app = express();
+const port = process.env.PORT;
+
+// app.use(cors());
+// app.use(passport.initialize());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(router);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `⚡️[${process.env.APP_NAME}]: Server is running at http://localhost:${port}`
+  );
+});
